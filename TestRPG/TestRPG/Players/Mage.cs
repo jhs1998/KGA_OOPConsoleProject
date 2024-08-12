7dcf0c4e079ce36bc66c29cbff8b5878df453078
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-08-12 15:31:22 +0900

    텍스트 RPG 제작

---
+++ TestRPG/TestRPG/Players/Mage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestRPG.Monsters;
+
+namespace TestRPG.Players
+{
+    public class Mage : Player
+    {
+        public Mage(string name)
+        {
+            this.name = name;
+            this.job = Job.Mage;
+            this.maxHP = 60;
+            this.curHP = maxHP;
+            this.attack = 50;
+            this.defense = 10;
+            this.gold = 100;
+        }
+
+        public override void Skill(Monster monster)
+        {
+            // TODO : 스킬 구현
+        }
+    }
+}
---
+++ TestRPG/TestRPG/Players/Warrior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestRPG.Monsters;
+
+namespace TestRPG.Players
+{
+    public class Warrior : Player
+    {
+        public Warrior(string name)
+        {
+            this.name = name;
+            this.job = Job.Warrior;
+            this.maxHP = 120;
+            this.curHP = maxHP;
+            this.attack = 30;
+            this.defense = 30;
+            this.gold = 0;
+        }
+
+        public override void Skill(Monster monster)
+        {
+            // TODO : 스킬 구현
+        }
+    }
+}
